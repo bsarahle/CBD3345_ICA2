a8d52d365c27972676e78248109b237d37ec794f
bsarahle <118011484+bsarahle@users.noreply.github.com>
2023-11-23 05:22:24 +0530

    Update run-tests.sh

--- docker-action/run-tests.sh
+++ docker-action/run-tests.sh
@@ -8,5 +8,3 @@ docker pull ghcr.io/bsarahle/cbd3345_ica2/python-app:latest
 
 # Run Docker Container with Tests and save output
 docker run --rm ghcr.io/bsarahle/cbd3345_ica2/python-app:latest python -m unittest test_app.py > test_output.txt 2>&1
-ls -ltr
-pwd
